a5655c863cae17cd416ae0e4d80d746d334104f5
Valerio <ilvalerione@gmail.com>
2017-11-20 14:38:40 +0100

    use service

--- config/force_https.php
+++ config/force_https.php
@@ -9,8 +9,13 @@ return [
     |
     | List here all APP_ENV that you want force redirect to https connection.
     |
+    | supported:
+    |   - '*' (star to always redirect)
+    |   - ['production'] (array of environment names)
+    |
     */
 
+    //'environments' => '*'
     'environments' => [
         // 'local', <-- usually no
         'development',
